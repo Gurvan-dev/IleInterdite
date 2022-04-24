3ab18e96b75478408fef01b1efaaa4737cde9452
Gurvan <gurvan@laptop-j21ubaim.home>
2022-04-24 16:42:28 +0200

    bazinga

---
+++ WindowManager.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.util.*;
+
+public class WindowManager {
+
+    static ArrayList<JFrame> openedWindow = new ArrayList<JFrame>();
+
+    static void closeAll() {
+        for (JFrame jp : openedWindow) {
+            jp.dispose();
+        }
+        openedWindow = new ArrayList<JFrame>();
+    }
+
+    static void LaunchMainMenu() {
+        closeAll();
+        MainMenu mm = new MainMenu();
+    }
+
+    static void LaunchGame() {
+        closeAll();
+        Modele modele = new Modele();
+        VueMain vueMain = new VueMain(modele);
+    }
+
+    static void LaunchWinMenu() {
+
+    }
+
+    static void LaunchLooseMenu() {
+
+    }
+}
